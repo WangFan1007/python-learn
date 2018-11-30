ad9e327dc187438318fb933e7a6ff064f2812b6e
FFine <ffine@FFinedeMacBook-Pro.local>
2018-11-30 16:19:19 +0800

    基本操作

---
+++ test/test.py
@@ -0,0 +1,30 @@
+#coding=utf-8
+message = "Hello Python"
+print(message)
+
+name = 'ada wang fan'
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+languange = ' python '
+print(languange.rstrip() + '!') 
+
+print(3/2)
+print(3 ** 3)
+age = 23
+print("Happy " + str(age) + "rd birthday")
+
+
+names = ['王凡','老左','老苟','hello']
+print(names)
+print(names[-1].title())
+
+arr = [1,2,3,4,5,9,8,6,7]
+
+arr.sort()
+print(arr)
+arr.sort(reverse=True)
+print(arr)
+print(sorted(arr))
+print(arr)
