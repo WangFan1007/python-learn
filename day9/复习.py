ed268dbdc0ba50405d56151c98a3d79503f5c833
dustfine <1036815077@qq.com>
2018-12-01 12:03:19 +0800

    cest

---
+++ day9/复习.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+
+import os
+
+path = os.getcwd()
+print(path)
+
+# file = open('text.log','r')
+
---
+++ test/main.py
@@ -0,0 +1,9 @@
+import dog
+
+my_dog = dog.Dog('willie',6)
+
+print("My dog's name is " + my_dog.name.title())
+
+my_dog.sit()
+my_dog.roll_over()
+
