53ad63ce21bfb0636d42182cb7648f3985b6997e
FFine <ffine@FFinedeMacBook-Pro.local>
2018-12-07 11:38:31 +0800

    some change

--- day30/client.py
+++ day30/client.py
@@ -1,13 +1,7 @@
 import socket
-
 sk = socket.socket()
-
 sk.connect(('127.0.0.1',8080))
-
 sk.send(b'hehe')
-
 ret = sk.recv(1024)
-
 print(ret)
-
 sk.close()
--- day30/server.py
+++ day30/server.py
@@ -1,19 +1,11 @@
 import socket
-
 sk = socket.socket()
-
 sk.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 sk.bind(('127.0.0.1',8080))
-
 sk.listen()
-
 conn,addr = sk.accept()
-
 ret = conn.recv(1024)
 print('server',ret)
-
 conn.send(b'hello !')
-
 conn.close()
-
 sk.close()
